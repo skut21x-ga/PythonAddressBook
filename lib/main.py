d002fca2cf85de998b29dc8ae3df9008557a9eb7
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-04 00:43:11 -0400

    added samples

--- lib/main.py
+++ lib/main.py
@@ -9,7 +9,6 @@ class BaseModel(Model):
         database = db
 
 class Contact(BaseModel):
-    id = CharField()
     first_name = CharField()
     last_name = CharField()
     area_code = IntegerField()
@@ -20,3 +19,10 @@ class Contact(BaseModel):
 db.connect()
 
 db.create_tables([Contact])
+
+scott = Contact(first_name='Scott', last_name='Kutler', area_code = 469, phone_number = 3870895)
+scott.save()
+noah = Contact(first_name='Noah', last_name='Clark', area_code = 202, phone_number = 5555555)
+noah.save()
+roger = Contact(first_name='Roger', last_name='Campbell', area_code = 202, phone_number = 5551234)
+roger.save()
