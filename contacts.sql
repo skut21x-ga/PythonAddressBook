243fff9e9c4dda8de1ec1a530717239fc5b80e08
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-03 22:34:42 -0400

    make contacts db file

---
+++ contacts.sql
@@ -0,0 +1,31 @@
+-- PostgreSQ Address Book
+
+BEGIN;
+
+    SET client_encoding
+    = 'LATIN1';
+
+CREATE TABLE contacts
+(
+    id integer NOT NULL,
+    first_name text NOT NULL,
+    last_name text NOT NULL,
+    area_code integer(3) NOT NULL,
+    phone_number integer(7) NOT NULL
+);
+
+-- Data for Contacts 
+COPY contacts
+(id, first_name, last_name, area_code, phone_number) FROM stdin;
+1	Scott   Kutler  469 3870895
+2	Noah    Clark   202 5552020	
+3	Roger   Campbell    202 5551010
+\.
+
+ALTER TABLE ONLY contacts
+ADD CONSTRAINT contacts_pkey PRIMARY KEY
+(id);
+
+COMMIT;
+
+ANALYZE contacts;
