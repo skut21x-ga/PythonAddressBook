7952654ac0c59ef309f5a429639fd21ac944991a
Scott Kutler <61331933+skut21x-ga@users.noreply.github.com>
2020-05-03 23:00:12 -0400

    setting up sql

--- contacts.sql
+++ contacts.sql
@@ -1,5 +1,3 @@
--- PostgreSQ Address Book
-
 BEGIN;
 
     SET client_encoding
@@ -10,8 +8,8 @@ CREATE TABLE contacts
     id integer NOT NULL,
     first_name text NOT NULL,
     last_name text NOT NULL,
-    area_code integer(3) NOT NULL,
-    phone_number integer(7) NOT NULL
+    area_code integer NOT NULL,
+    phone_number integer NOT NULL
 );
 
 -- Data for Contacts 
@@ -22,10 +20,11 @@ COPY contacts
 3	Roger   Campbell    202 5551010
 \.
 
+
 ALTER TABLE ONLY contacts
 ADD CONSTRAINT contacts_pkey PRIMARY KEY
 (id);
 
 COMMIT;
 
-ANALYZE contacts;
+-- ANALYZE contacts
